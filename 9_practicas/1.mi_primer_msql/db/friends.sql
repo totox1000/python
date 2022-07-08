7b9a8af01c1b55ed044a55d489ef3c0f5f2bec97
Andres <andresbcrr@gmail.com>
2022-07-08 16:14:51 -0400

    se crea la carpeta practicas

---
+++ 9_practicas/1.mi_primer_msql/db/friends.sql
@@ -0,0 +1,4 @@
+# INSERT INTO first_flask.friends (first_name, last_name, occupation)
+# VALUES ("Adrien", "Dion", "Curriculum Developer") , ("Mr.Nibbles", "Pancakes", "Horse");
+
+SELECT * FROM first_flask.friends;
---
+++ 9_practicas/1.mi_primer_msql/primer_flask_mysql/friend.py
@@ -0,0 +1,32 @@
+# importar la función que devolverá una instancia de una conexión
+from mysqlconnection import connectToMySQL
+# modelar la clase después de la tabla friend de nuestra base de datos
+class Friend:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.occupation = data['occupation']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    # ahora usamos métodos de clase para consultar nuestra base de datos
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM friends;"
+        # asegúrate de llamar a la función connectToMySQL con el esquema al que te diriges
+        results = connectToMySQL('first_flask').query_db(query)
+        # crear una lista vacía para agregar nuestras instancias de friends
+        friends = []
+        # Iterar sobre los resultados de la base de datos y crear instancias de friends con cls
+        for friend in results:
+            friends.append( cls(friend) )
+        return friends
+
+        # ...otros métodos de clase
+        # método de clase para guardar a nuestro amigo en la base de datos
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        # data es un diccionario que se pasará al método de guardar desde server.py
+        return connectToMySQL('first_flask').query_db( query, data )
+            
