2d0f5b629fea26a36fc5d3aea93eb053102b6de2
Andres <andresbcrr@gmail.com>
2022-07-11 23:51:50 -0400

    se agregan mas ejercicios

---
+++ 7_mysql_flask/Dojos_y_ninjas/db/forma_de_borrar.sql
@@ -0,0 +1,10 @@
+SELECT * FROM dojo_ninjas.dojos;
+
+delete from dojos where id <>1;
+
+describe dojos;
+
+
+delete from ninjas where id <> 2;
+
+select * from ninjas;
---
+++ 7_mysql_flask/Muro_privado/db/query.sql
@@ -0,0 +1,2 @@
+SELECT * FROM users;
+SELECT users.first_name as sender, users2.first_name as receiver, messages.* FROM users LEFT JOIN messages ON users.id = messages.sender_id LEFT JOIN users as users2 ON users2.id = messages.receiver_id WHERE users2.id = 2;
